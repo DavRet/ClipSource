46db2247d8a7ce4d451437fe6c6d9540fb921d57
DavRet <david.r@appmeisterei.de>
2017-09-23 19:34:55 +0200

    Added Image Source and File Source Path

--- init.py
+++ init.py
@@ -5,12 +5,10 @@ import exifread
 import win32con
 from bs4 import BeautifulSoup
 
-import win32clipboard
 import HTMLClipboard
-
-
-
-
+import os
+import os.path
+import sys
 
 from PySide import QtGui, QtCore
 
@@ -19,63 +17,30 @@ clipboard = app.clipboard()
 
 clips = []
 
-
-try:
-    import tkinter as tk  # for python 3
-except:
-    import Tkinter as tk  # for python 2
 import pygubu
 from PyQt4 import QtGui, uic
 
-
+import win32clipboard as clp, win32api
 
 
 class Application:
     def __init__(self, master):
-
-        #1: Create a builder
+        # 1: Create a builder
         self.builder = builder = pygubu.Builder()
 
-        #2: Load an ui file
+        # 2: Load an ui file
         builder.add_from_file('ClipSourceUI.ui')
 
-        #3: Create the widget using a master as parent
+        # 3: Create the widget using a master as parent
         self.mainwindow = builder.get_object('LabelFrame_3', master)
-
 
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         uic.loadUi('clipui.ui', self)
-
-
         self.show()
 
-    def test(self):
-        print "test"
-        # def initUI(self):
-    #     QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
-    #
-    #     self.setToolTip('This is a <b>QWidget</b> widget')
-    #
-    #     btn = QtGui.QPushButton('Button', self)
-    #     btn.setToolTip('This is a <b>QPushButton</b> widget')
-    #     btn.resize(btn.sizeHint())
-    #     btn.move(50, 50)
-    #
-    #     self.setGeometry(1100, 200, 500, 800)
-    #     self.setWindowTitle('Tooltips')
-    #     self.show()
-    #
-    #
-    # def _populate(self):
-    #
-    #     self.clear()
-    #
-    #     for clip in clips:
-    #         item = QtGui.QListWidgetItem(self)
-    #         item.setText(clip)
 
 window = Window()
 
@@ -85,43 +50,52 @@ content_model = QtGui.QStandardItemModel(content_list)
 source_list = window.source_list
 source_model = QtGui.QStandardItemModel(source_list)
 
+source_radio = window.source_radio
+
+auto_source_enabled = False
+
 
 def main():
-
     clipboard.dataChanged.connect(clipboardChanged)
-
-    #window = Window()
-
 
     content_list.setModel(content_model)
     source_list.setModel(source_model)
 
+    source_radio.toggled.connect(sourceRadioClicked)
 
-    # We set the form to be our ExampleApp (design)
-
-    #form.content_list.setModel(model)
     window.show()
-
-    #list.setWindowTitle('ClipSource')
-    #list.setMinimumSize(600, 400)
-
-
-    #list.show()
 
     setModelForList()
 
-    #root = tk.Tk()
-    #app = Application(root)
-    #root.mainloop()
+    content_list.clicked.connect(contentClick)
+    source_list.clicked.connect(sourceClick)
+
+    # root = tk.Tk()
+    # app = Application(root)
+    # root.mainloop()
 
 
 
     sys.exit(app.exec_())
 
 
+def contentClick(index):
+    item = content_model.item(index.row(), index.column())
+    unicode_item = unicode(item.text(), "utf-8")
+    clipboard.setText(unicode_item)
+
+
+def sourceClick(index):
+    item = source_model.item(index.row(), index.column())
+    unicode_item = unicode(item.text(), "utf-8")
+    clipboard.setText(unicode_item)
+
+
+def sourceRadioClicked(enabled):
+    print "clicked"
+
+
 def setModelForList():
-
-
     for clip in clips:
         clipText = QtGui.QStandardItem(clip[0])
         source = QtGui.QStandardItem(clip[1])
@@ -130,12 +104,12 @@ def setModelForList():
         source_model.appendRow(source)
 
     def on_item_changed(item):
+        print "changed"
         if not item.checkState():
             return item
         print item.text()
 
         clipboard.setText(item.text())
-
 
         i = 0
         while content_model.item(i):
@@ -154,60 +128,86 @@ def buttonClick():
 
 
 def clipboardChanged():
-
     mimeData = clipboard.mimeData()
-
-    image = clipboard.image()
-
-    print image.height()
-
-    image.save("C:\\Users\\David\\Documents\\Images\\aaa.png", "PNG", -1)
-
-    f = open("tiger.jpg", 'rb')
-
-    tags = exifread.process_file(f)
-
-    print tags
-
-    #print image.height()
 
 
     if HTMLClipboard.HasHtml():
-        # print('there is HTML!!')
         source = HTMLClipboard.GetSource()
         print(source)
 
-        #url = QtCore.QUrl(source)
-        #mimeData.setUrls(url)
-        #print clipboard.mimeData().urls()
+        if (source == None):
+            html = ""
+            clp.OpenClipboard(None)
+            rc = clp.EnumClipboardFormats(0)
+            while rc:
+                try:
+                    format_name = clp.GetClipboardFormatName(rc)
+                except win32api.error:
+                    format_name = "?"
+                try:
+                    format = clp.GetClipboardData(rc)
+                except win32api.error:
+                    format = "?"
+                if (format_name == "HTML Format"):
+                    html = format
+                rc = clp.EnumClipboardFormats(rc)
+
+            clp.CloseClipboard()
+            soup = BeautifulSoup(html, "html.parser")
+
+            link =  soup.find('img')['src']
+            print link
+            source = link
 
         originalText = clipboard.text()
+
+        if originalText == None or originalText == "":
+            originalText = "Image"
 
         clips.append(tuple((originalText, source)))
 
         setModelForList()
 
-        #clipboard.setText(originalText + "\n" + "Copied from:\n"  + source)
+        if (source_radio.isChecked()):
+            wrap_with_comment = "hash"
+
+            if (wrap_with_comment == "hash"):
+                clipboard.setText(originalText + "\n" + "# Copied from:\n" + "# " + source)
+            elif (wrap_with_comment == "slash"):
+                clipboard.setText(originalText + "\n" + "// Copied from:\n" + "// " + source)
+            else:
+                clipboard.setText(originalText + "\n" + "Copied from:\n" + source)
 
         print clipboard.text()
     else:
-        print('no html')
+        clp.OpenClipboard(None)
+
+        rc = clp.EnumClipboardFormats(0)
+        while rc:
+            try:
+                format_name = clp.GetClipboardFormatName(rc)
+            except win32api.error:
+                format_name = "?"
+            # print "format", rc, format_name
+            try:
+                format = clp.GetClipboardData(rc)
+            except win32api.error:
+                format = "?"
+            if (format_name == 'FileNameW'):
+                print "File Source Path: \n", format
+            rc = clp.EnumClipboardFormats(rc)
+
+        clp.CloseClipboard()
 
     soup = BeautifulSoup(mimeData.html(), "html.parser")
 
     links = soup.find_all('a')
 
-
-
-
-
-
-    #print links
+    # print links
     for tag in links:
         link = tag.get('href', None)
         if link is not None:
             print link
-
 
 
 if __name__ == '__main__':
